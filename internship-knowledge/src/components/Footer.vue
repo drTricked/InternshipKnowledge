2a861205f073507ac8265fe967538264b06074d8
drTricked <markdavid1009@hotmail.co.uk>
2021-04-11 10:15:42 -0400

    Adds initial Header and Footer

---
+++ internship-knowledge/src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="6" offset="3">
+      <!-- need to change this into links -->
+      <!-- TODO: set up links -->
+      <v-btn text>email</v-btn>
+      <v-btn text>linkedin</v-btn>
+      <v-btn text>github</v-btn>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style></style>
---
+++ internship-knowledge/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app-bar app dense color="primary" dark>
+    <div class="d-flex align-center">
+      <v-btn text>
+        <router-link class="r-link" to="/">Home</router-link>
+      </v-btn>
+      <v-btn text>
+        <router-link class="r-link" to="/About">About</router-link>
+      </v-btn>
+      <v-btn text>
+        <router-link class="r-link" to="/Blog">Blog</router-link>
+      </v-btn>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      href="https://github.com/vuetifyjs/vuetify/releases/latest"
+      target="_blank"
+      text
+    >
+      <span class="mr-2">Latest Release</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .r-link {
+    color: white;
+    text-decoration: none;
+  }
+</style>
