ab573b0a3af13ff56eea4fa018dbfdce63a50e18
drTricked <markdavid1009@hotmail.co.uk>
2021-04-20 19:01:01 -0400

    updates : added 2 blog posts

--- internship-knowledge/src/views/Blog.vue
+++ internship-knowledge/src/views/Blog.vue
@@ -41,13 +41,29 @@
       posts: [
         {
           title: "First Blog Post!",
-          date: new Date(),
+          date: new Date("2021-04-11"),
           text: "This is my first blog post!!!",
         },
         {
           title: "Second Blog Post!",
-          date: new Date(),
+          date: new Date("2021-04-11"),
           text: "Second blog post, lets gooooo!!!",
+        },
+        {
+          title: "April 20 Update",
+          date: new Date("2021-04-20"),
+          text:
+            "Been a couple days since I worked on this website. I have done some practice with Java spring recently, I wanted to get familiar with the technology before hand." +
+            " Java spring allows you to write the backend using java. From what I have heard JDBC isn't very nice to work with; interestingly we used it in our databases class." +
+            " Not too sure what I should be working on right now. I want to start working on some front end stuff, but there is always more backend to learn." +
+            " Ideally I should be starting to work on something that combines all of my knowledge together.",
+        },
+        {
+          title: "April 20 Update pt2",
+          date: new Date("2021-04-20"),
+          text:
+            "Recap: tried using Vue.js, and Java spring. Starting to attempt some blockchain sounds very interesting? Maybe going back and working on a MERN stack would be good." +
+            " I am unsure of working on backend with Java so far? Hard to find resources for a Vuejs + Java Spring stack. Plus I still enjoy working on design.",
         },
       ],
     }),
